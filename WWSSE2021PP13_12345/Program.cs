e6540e6514ad94c4769769d4298654191e0ef3bf
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-10 14:58:06 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP13_12345/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+    Napisz program obliczający sumę n kolejnych liczb całkowitych zawartych pomiędzy 
+    wartościami minimum i maksimum wprowadzonymi przez operatora.
+*/
+
+int value_min, value_max, sum = 0;
+
+Console.Write("Wprowadź wartość minimum: ");
+value_min = int.Parse(Console.ReadLine());
+
+Console.Write("Wprowadź wartość maksimum: ");
+value_max = int.Parse(Console.ReadLine());
+
+for (int i = value_min; i <= value_max; i++)
+{
+    sum += i;
+}
+Console.WriteLine("Suma liczb wynosi: {0}.", sum);
